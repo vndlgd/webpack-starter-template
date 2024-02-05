976362c95789d8a95de76ebdaadd78fc47bf1c5c
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2024-02-04 23:11:56 -0800

    Uninstall and Remove CleanWebpackPlugin from webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -38,7 +37,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/index.html'),
